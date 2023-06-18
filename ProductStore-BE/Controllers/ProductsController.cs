0b3d049bdec68738979b14a21597fd11e2b8ed59
thatsBruno <brunoandreribeiro18@gmail.com>
2023-06-18 18:43:17 +0100

    Add project files.

---
+++ ProductStore-BE/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProductStore_BE.Data;
+using ProductStore_BE.Models;
+
+namespace ProductStore_BE.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class ProductsController : Controller
+{
+    private readonly ProductStoreDbContext _dbContext;
+    public ProductsController(ProductStoreDbContext productStoreDbContext)
+    {
+        _dbContext = productStoreDbContext;
+    }
+    [HttpGet]
+    public async Task<IActionResult> GetProducts()
+    {
+        var products = await _dbContext.Products.ToListAsync();
+
+        return Ok(products);
+    }
+    [HttpPost]
+    public async Task<IActionResult> AddProduct([FromBody] Product product)
+    {
+        product.Id = Guid.NewGuid();
+
+        await _dbContext.Products.AddAsync(product);
+
+        await _dbContext.SaveChangesAsync();
+
+        return Ok();
+    }
+}
---
+++ ProductStore-BE/Data/ProductStoreDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductStore_BE.Models;
+
+namespace ProductStore_BE.Data;
+
+public class ProductStoreDbContext : DbContext
+{
+    public ProductStoreDbContext(DbContextOptions options): base(options)
+    {
+        
+    }
+    public DbSet<Product> Products { get; set; }
+}
